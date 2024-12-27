caa5f3dd14785e22dafc717ddf935a7960b4e447
Alwin Rajkumar <141786788+alwnraj@users.noreply.github.com>
2024-12-27 17:11:49 -0500

    passed exercise 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(a, b) {
+    let result = '';
+    for(let i=0; i< b; i++){
+        result += a;
+        
+    }
+    return result;
 };
 
+//console.log(repeatString('hey',5));
 // Do not edit below this line
 module.exports = repeatString;
