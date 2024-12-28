aafb0801889852078d5d76785d5d5232cea3d993
Alwin Rajkumar <141786788+alwnraj@users.noreply.github.com>
2024-12-28 18:41:48 -0500

    exercise 3 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(a) {
+    const splita = a.split('').reverse().join('');
+    return splita;
 };
 
+//console.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;
