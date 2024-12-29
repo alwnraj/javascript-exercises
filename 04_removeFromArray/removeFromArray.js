51cf52ccc27721e32432d9196803c1239bb10124
Alwin Rajkumar <141786788+alwnraj@users.noreply.github.com>
2024-12-29 17:42:36 -0500

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(a,b) {
+
+    let newarr = [];
+    
+    for(let i=0 ; i < a.length; i++){
+        if(a[i] !== b){
+            newarr.push(a[i]);
+        }
+    }
+    return newarr;
 };
 
+//console.log(removeFromArray([1, 2, 3, 4],2));
 // Do not edit below this line
 module.exports = removeFromArray;
