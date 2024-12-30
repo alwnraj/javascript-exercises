c045880aa0ae6357083f3387f8bab81eefcb7600
Alwin Rajkumar <141786788+alwnraj@users.noreply.github.com>
2024-12-29 19:49:58 -0500

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    let newsum = 0;
+    for (let i=start; i<end+1; i++){
+        newsum += i;
+    }
+
+    return newsum;
 
 };
 
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
