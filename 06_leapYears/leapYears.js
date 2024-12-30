57f2d0845139fd0b209d4b8eef4030a7976bb60f
Alwin Rajkumar <141786788+alwnraj@users.noreply.github.com>
2024-12-30 18:25:07 -0500

    finished exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(number) {
+    if(number%4 ==0 && number%400 != 0){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
+//console.log(leapYears(2000));
 // Do not edit below this line
 module.exports = leapYears;
