1eb95730bc838f5711e468dda307014ea77712f0
Alwin Rajkumar <141786788+alwnraj@users.noreply.github.com>
2024-12-30 19:20:37 -0500

    finished exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+  const result = ((a - 32) * (5/9));
+  return parseFloat(result.toFixed(2));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a) {
+  const result = (((a * (9/5))+32))
+  return parseFloat(result.toFixed(2))
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(32));
 
 // Do not edit below this line
 module.exports = {
